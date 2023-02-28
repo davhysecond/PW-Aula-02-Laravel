e8893581d7a80edf5fe0aa7b93b5004b5a59aa85
Davhy Andrade <david.souza95@etec.sp.gov.br>
2023-02-28 16:04:06 -0300

    first commit

---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Produtos</title>
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+  <nav class="menu">
+    <div class="position">
+      <a href="/">Home</a>
+      <a href="/empresa">Empresa</a>
+      <a href="/produtos">Produtos</a>
+      <a href="/contato">Contato</a>
+    </div>
+  </nav>
+  <section>
+    <div class="position">
+      <h1>Page Produtos</h1>
+    </div>
+  </section>
+</body>
+</html>
